780d8656a174c8c1094cffd7b06d4845c5317b17
Julian lange <julian.laenge@gmail.com>
2021-05-26 15:28:35 +0300

    working with refs instead of events to be able to manipulate element from event of different element

--- src/components/ChatComposer.vue
+++ src/components/ChatComposer.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="chat-message-container">
     <!-- <div id="chat-message" data-ph="Type a message" contentEditable="true" v-on:change="onChange">{{chatMessage}}</div> -->
-    <textarea id="chat-message" placeholder="Type a message" v-on:keyup="resizeTextarea" @change="onChange" :value="chatMessage" />
-    <font-awesome-icon icon="paper-plane" size="2x" v-on:click="$emit('get-message')" />
+    <textarea id="chat-message" ref="textarea" placeholder="Type a message" v-on:keyup="resizeTextarea" @change="onChange" :value="chatMessage" />
+    <font-awesome-icon icon="paper-plane" size="2x" v-on:click="onClick" />
   </div>
 </template>
 
@@ -15,6 +15,11 @@ export default {
       }
     },
   methods: {
+    onClick() {
+      this.$emit('get-message');
+      // The line below resets the height of the textarea which has been modified to expand based on content
+      this.$refs.textarea.style.height = "2.5rem";
+    },
     onChange (e) {
       // This is for training purposes only, there is no need to emit to parent. All can be handle here..
       this.$emit('updateMessage', e.target.value)
@@ -22,12 +27,10 @@ export default {
     alert(msg) {
       alert(msg)
     },
-    resizeTextarea(e) {
-      // console.log(e.target)
-      e.target.style.height = "1px";
-      e.target.style.height = (e.target.scrollHeight)+"px";
+    resizeTextarea() {
+      this.$refs.textarea.style.height = "1px";
+      this.$refs.textarea.style.height = (this.$refs.textarea.scrollHeight)+"px";
     }
-
   },
   props: ['chatMessage'],
   
@@ -48,7 +51,6 @@ This is for the div version to simulate textarea behaviour
   padding: 1rem 2rem;
   order: 3;
   position: relative;
-  min-height: 60px;
   border: 1px solid #dee2e6;
 }
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { Auth0Plugin } from "./auth";
-import hljs from "highlight.js/lib/core";
-import "highlight.js/styles/github.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -16,8 +14,6 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { domain, clientId } from "../auth_config.json";
 
 Vue.config.productionTip = false;
-
-Vue.use(hljs.vuePlugin);
 
 Vue.use(Auth0Plugin, {
   domain,
