ecd4efc372a1909ca29cc79e69376c052ca2989b
Julian lange <julian.laenge@gmail.com>
2021-05-28 00:16:53 +0300

    Added mongoose and connected to mongoDB

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,13 @@
-import express from "express";
-import { json } from "body-parser";
-
+import dotenv from "dotenv";
+import express, { json } from "express";
+// previously >> import { json } from "body-parser";
 import conversationRoutes from "./routes/conversations";
+import { connectDB } from "./db/db";
+import colors from "colors";
+
+dotenv.config();
+connectDB();
+colors.enable();
 
 const app = express();
 
@@ -18,11 +24,16 @@ app.use(
   ) => {
     res.status(500).json({
       message: err.message,
-      additional: "C'mon man, just follow the rules!",
     });
   }
 );
 
-app.listen(5000, () => {
-  console.log("Server listening on port 5000");
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`.cyan);
+});
+
+// Will stop the program if unhandled exception, here to stop if there's a db auth problem
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${(err as { message: string }).message}`.red);
+  server.close(() => process.exit(1));
 });
